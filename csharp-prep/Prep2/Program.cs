150bcfe5bf6e2f136b21f53693ba3e8302a7439b
Juan Suarez <soaders5000@gmail.com>
2023-04-22 23:31:43 -0500

    Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        String letter = "";
+        Console.WriteLine("What is you grade percentage? ");
+        String grade = Console.ReadLine();
+        int number = int.Parse(grade);
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"your letter grade is: {letter}");
     }
 }
