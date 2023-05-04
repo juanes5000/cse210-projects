b9b228cc4747a207529d6e4a1b3d5b8b6928f5f6
Juan Suarez <soaders5000@gmail.com>
2023-05-03 22:41:15 -0500

    Finished Learning02

---
+++ prepare/Learning02/job.cs
@@ -0,0 +1,13 @@
+public class Job
+{
+    public String _company = "";
+    public String _jobTitle = "";
+    public int _startYear = 0;
+    public int  _endYear = 0;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+
+}
